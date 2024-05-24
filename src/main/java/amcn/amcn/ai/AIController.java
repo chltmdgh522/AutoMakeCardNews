d55f485775ea58d89be0e9c8f204e2399173c8ad
seung_ho_choi.s <chltmdgh517@naver.com>
2024-05-24 21:35:42 +0900

    open ai dall-e-3 모델 사용 성공

--- src/main/java/amcn/amcn/ai/AIController.java
+++ src/main/java/amcn/amcn/ai/AIController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.io.IOException;
+
 @Controller
 @Slf4j
 @RequiredArgsConstructor
@@ -17,13 +19,13 @@ public class AIController {
     private final AIService aiService;
 
     @GetMapping("/ai-image")
-    public String getImage(Model model){
-        String apple = aiService.generatePicture("apple");
-        model.addAttribute("url",apple);
+    public String getImage(Model model) throws IOException, InterruptedException {
+        String prompt = aiService.generatePictureV2("눈내리고 있고 크리스마스 분위기나는 이미지 좀 생성해줘");
+        model.addAttribute("url", prompt);
         return "test";
     }
     @PostMapping("/ai-image")
-    public ResponseEntity<?> generateImage(@RequestBody String prompt) {
-        return new ResponseEntity<>(aiService.generatePicture(prompt), HttpStatus.OK);
+    public ResponseEntity<?> generateImage(@RequestBody String prompt) throws IOException, InterruptedException {
+        return new ResponseEntity<>(aiService.generatePictureV2(prompt), HttpStatus.OK);
     }
 }
