374121a56ac8b23a5836d951dcfb794dc36f8769
chltmdgh522 <chltmdgh517@naver.com>
2024-06-26 22:22:15 +0900

    커뮤니티 구축

---
+++ src/main/java/amcn/amcn/commonentity/web/CommunityController.java
@@ -0,0 +1,56 @@
+package amcn.amcn.commonentity.web;
+
+import amcn.amcn.member.domain.member.Member;
+import amcn.amcn.member.repository.MemberRepository;
+import amcn.amcn.member.web.session.SessionConst;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class CommunityController {
+
+    private final MemberRepository memberRepository;
+    @GetMapping("/community")
+    public String getCommunity(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
+                                 Member loginMember, Model model) {
+
+        Optional<Member> findMember = memberRepository.findMemberId(loginMember.getMemberId());
+        if (findMember.isPresent()) {
+            Member member = findMember.get();
+            model.addAttribute("type", member.getRoleType().name());
+            model.addAttribute("member", member);
+        } else {
+            return null;
+        }
+
+
+        return "community/community";
+    }
+
+    @GetMapping("/community/more")
+    public String getCommunityMore(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
+                               Member loginMember, Model model) {
+
+        Optional<Member> findMember = memberRepository.findMemberId(loginMember.getMemberId());
+        if (findMember.isPresent()) {
+            Member member = findMember.get();
+            model.addAttribute("type", member.getRoleType().name());
+            model.addAttribute("member", member);
+        } else {
+            return null;
+        }
+
+
+        return "community/communitymore";
+    }
+
+}
---
+++ src/main/java/amcn/amcn/commonentity/web/CommunityCrud.java
@@ -0,0 +1,37 @@
+package amcn.amcn.commonentity.web;
+
+import amcn.amcn.member.domain.member.Member;
+import amcn.amcn.member.repository.MemberRepository;
+import amcn.amcn.member.web.session.SessionConst;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class CommunityCrud {
+
+    private final MemberRepository memberRepository;
+    @GetMapping("/community/add")
+    public String getCommunity(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
+                               Member loginMember, Model model) {
+
+        Optional<Member> findMember = memberRepository.findMemberId(loginMember.getMemberId());
+        if (findMember.isPresent()) {
+            Member member = findMember.get();
+            model.addAttribute("type", member.getRoleType().name());
+            model.addAttribute("member", member);
+        } else {
+            return null;
+        }
+
+
+        return "community/communityadd";
+    }
+}
