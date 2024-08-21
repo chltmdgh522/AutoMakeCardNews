814114ca4ad145f47eae50db604db2ff97395234
chltmdgh522 <chltmdgh517@naver.com>
2024-08-21 20:21:22 +0900

    members 커뮤니티 부분 잇기

--- src/main/java/amcn/amcn/other/service/OthersService.java
+++ src/main/java/amcn/amcn/other/service/OthersService.java
@@ -20,61 +20,63 @@ public class OthersService {
 
     private final OthersRepository othersRepository;
 
-    public List<Board> postBoard(Member loginMember){
+    public List<Board> postBoard(Member loginMember) {
         List<Board> boards = othersRepository.findPostBoard(loginMember);
         List<Board> boards_copy = new ArrayList<>();
 
         return titleSubstnaceModify(boards, boards_copy);
     }
 
-    public List<Comment> commentBoard(Member loginMember){
+    public List<Comment> commentBoard(Member loginMember) {
         List<Comment> comments = othersRepository.findCommentBoard(loginMember);
         List<Comment> comments_copy = new ArrayList<>();
 
         return commentSubstnaceModify(comments, comments_copy);
     }
 
-    public List<Board> likeBoard(Member loginMember){
-        List<Board> boards =new ArrayList<>();
+    public List<Board> likeBoard(Member loginMember) {
+        List<Board> boards = new ArrayList<>();
         for (Likes likes : othersRepository.findHeartBoard(loginMember)) {
-           Board newBoard=new Board();
-           try{
-               newBoard.setBoardId(likes.getBoard().getBoardId());
-               newBoard.setComments(likes.getBoard().getComments());
-               newBoard.setMember(likes.getMember());
-               newBoard.setLikes(likes.getBoard().getLikes());
 
-               if (likes.getBoard().getTitle().length() >= 7) {
-                   newBoard.setTitle(likes.getBoard().getTitle().substring(0, 7) + "...");
-               } else {
-                   newBoard.setTitle(likes.getBoard().getTitle());
-               }
+            Board newBoard = new Board();
+            try {
+                newBoard.setBoardId(likes.getBoard().getBoardId());
+                newBoard.setComments(likes.getBoard().getComments());
+                newBoard.setMember(likes.getMember());
+                newBoard.setLikes(likes.getBoard().getLikes());
 
-               if (likes.getBoard().getSubstance().length() >= 15) {
-                   newBoard.setSubstance(likes.getBoard().getSubstance().substring(0, 15) + "...");
-               } else {
-                   newBoard.setSubstance(likes.getBoard().getSubstance());
-               }
-               boards.add(newBoard);
 
-           }catch (NullPointerException e){
-               log.info(e.toString());
-           }
+                if (likes.getBoard().getTitle().length() >= 7) {
+                    newBoard.setTitle(likes.getBoard().getTitle().substring(0, 7) + "...");
+                } else {
+                    newBoard.setTitle(likes.getBoard().getTitle());
+                }
+
+                if (likes.getBoard().getSubstance().length() >= 15) {
+                    newBoard.setSubstance(likes.getBoard().getSubstance().substring(0, 15) + "...");
+                } else {
+                    newBoard.setSubstance(likes.getBoard().getSubstance());
+                }
+                boards.add(newBoard);
+
+            } catch (NullPointerException e) {
+                log.info(e.toString());
+            }
 
         }
 
+        for (Board board : boards) {
+            log.info(board.getTitle());
+        }
         return boards;
     }
 
 
-
-
-
-    public List<CardNews> findHeartCardNewsService(Member loginMember){
+    public List<CardNews> findHeartCardNewsService(Member loginMember) {
         return othersRepository.findHeartCardNews(loginMember);
     }
 
-    public List<CardNews> findForkCardNewsService(Member loginMember){
+    public List<CardNews> findForkCardNewsService(Member loginMember) {
         return othersRepository.findForkCardNews(loginMember);
     }
 
@@ -108,12 +110,10 @@ public class OthersService {
 
     private List<Comment> commentSubstnaceModify(List<Comment> comments, List<Comment> comments_copy) {
         for (Comment comment : comments) {
-            Comment newComment=new Comment();
+            Comment newComment = new Comment();
             newComment.setCommentId(comment.getCommentId());
             newComment.setDate(comment.getDate());
             newComment.setMember(comment.getMember());
-            newComment.setBoard(comment.getBoard());
-
 
 
             if (comment.getSubstance().length() >= 15) {
@@ -121,6 +121,14 @@ public class OthersService {
             } else {
                 newComment.setSubstance(comment.getSubstance());
             }
+
+            Board board = comment.getBoard();
+            if (comment.getBoard().getTitle().length() >= 7) {
+                board.setTitle(board.getTitle().substring(0, 7) + "...");
+            } else {
+                board.setTitle(board.getTitle());
+            }
+            newComment.setBoard(board);
 
             comments_copy.add(newComment);
         }
