14ef5118b42857aad077b5941338a34798f300cd
chltmdgh522 <chltmdgh517@naver.com>
2024-10-05 15:39:18 +0900

    소켓 구성 파일 변경2

--- src/main/java/amcn/amcn/config/SocketConfig.java
+++ src/main/java/amcn/amcn/config/SocketConfig.java
@@ -19,7 +19,7 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("https://amcn.kr/")
                 .withSockJS();
     }
 
