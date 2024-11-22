8bab3d585b8618a3ed850a5793bb4c722ea700e0
chltmdgh522 <chltmdgh517@naver.com>
2024-11-22 15:55:41 +0900

    fix: 디자인 수정6

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -171,7 +171,7 @@ function addTextToCanvas() {
         size: textSize,
         weight: fontWeight,
         font: fontFamily,
-        x: 30,
+        x: 60,
         y: 660
     });
     redrawCanvas();
